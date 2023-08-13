6293b0d25214c56f34c52a973b2fdb94acc2ed99
Rodrigo Zietlow <rodrigo.zietlow@gmail.com>
2023-08-12 23:56:09 -0300

    Validando se produto existe e adicionando códigos HTTP adequados

--- api/produto/index.js
+++ api/produto/index.js
@@ -3,11 +3,15 @@ const controllerProdutos = require("./mocks");
 module.exports = (server) => {
   // listar produtos
   server.get("/produto", (req, res) => {
-    res.send(controllerProdutos.listarProdutos());
+    res.status(200).send(controllerProdutos.listarProdutos());
   });
 
   // listar o produto
   server.get("/produto/:idProduto", (req, res) => {
-    res.send(controllerProdutos.buscarProduto(req.params.idProduto));
+    const produtoEncontrado = controllerProdutos.buscarProduto(
+      req.params.idProduto
+    );
+    if (produtoEncontrado) res.status(200).send(produtoEncontrado);
+    else res.status(404).send({ error: "Produto não encontrado" });
   });
 };
--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const port = 3000;
 const apiProdutos = require("./api/produto/index");
 
 app.get("/", (req, res) => {
-  res.send(
-    "Raiz da API. Idealmente aqui tem apenas roteamento para outras partes da API"
-  );
+  res
+    .status(200)
+    .send(
+      "Raiz da API. Idealmente aqui tem apenas roteamento para outras partes da API"
+    );
 });
 
 apiProdutos(app); // adiciona os endpoints que criarmos dentro da pasta de produto
