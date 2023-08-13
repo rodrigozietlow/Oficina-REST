d1dace7f667ae5cb8b6a509d11311d61723c41a5
Rodrigo Zietlow <rodrigo.zietlow@gmail.com>
2023-08-12 23:46:33 -0300

    Adicionando busca (parcial) e limpando código

--- api/produto/index.js
+++ api/produto/index.js
@@ -1,7 +1,16 @@
 const produtos = require("./mocks").produtos;
 
+const listarProdutos = () => produtos;
+const buscarProduto = (id) => produtos.find((produto) => produto.id == id);
+
 module.exports = (server) => {
+  // listar produtos
   server.get("/produto", (req, res) => {
-    res.send(produtos);
+    res.send(listarProdutos());
+  });
+
+  // listar o produto
+  server.get("/produto/2", (req, res) => {
+    res.send(buscarProduto(2));
   });
 };
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const apiProdutos = require("./api/produto/index");
 
 app.get("/", (req, res) => {
   res.send(
     "Raiz da API. Idealmente aqui tem apenas roteamento para outras partes da API"
   );
 });
+
+apiProdutos(app); // adiciona os endpoints que criarmos dentro da pasta de produto
 
 app.listen(port, () => {
   console.log(`Server up, porta ${port}`);
